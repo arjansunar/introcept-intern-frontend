3ed91a8672901b10ee9aeadf049c385ac36e4449
arjansunar <arjan.gahatrajsunar@gmail.com>
2022-03-13 14:54:55 +0545

    buttons for increment, decrement and reset

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,17 @@ import TailwindcssLogo from "./tailwindcss-logo.svg";
 function App() {
   const [count, setCount] = useState(0);
 
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
   return (
     <div className="w-screen h-screen flex items-center justify-center bg-gray-800 text-white">
       <div className="fixed inset-0 flex items-center justify-center bg-blend-darken">
@@ -13,55 +24,43 @@ function App() {
           {count}
         </p>
       </div>
-      <header className="flex flex-col items-center justify-center gap-4 z-10">
-        <div className="flex flow-row gap-12 items-center justify-center p-8">
-          <img src={ViteLogo} className="h-40" alt="logo" />
-          <img src={ReactLogo} className="h-48" alt="logo" />
-          <img src={TailwindcssLogo} className="h-48" alt="logo" />
+      <header className="flex flex-col items-center absolute top-24">
+        <h2 className="italic text-5xl uppercase">The Stack</h2>
+        <div className="flex space-x-3 mt-2">
+          <img className="h-20 " src={ReactLogo} alt="" />
+          <img className="h-20 " src={TailwindcssLogo} alt="" />
+          <img className="h-20 " src={ViteLogo} alt="" />
         </div>
-        <p className="font-bold text-4xl">Hello Vite + React + Tailwindcss!</p>
-        <p>
+      </header>
+      <main className="flex flex-col items-center justify-center gap-4 z-10">
+        <p className="font-bold text-4xl">Counter </p>
+        <p className="space-x-3">
           <button
             type="button"
-            onClick={() => setCount((count) => count + 1)}
+            onClick={increment}
+            className="px-4 py-2 my-4 rounded-lg bg-indigo-500 text-xl font-medium hover:bg-indigo-600 active:ring-4 ring-0 ring-indigo-500 ring-opacity-20"
+            aria-label="Increment button"
+          >
+            Increment
+          </button>
+          <button
+            type="button"
+            onClick={reset}
+            className="px-4 py-2 my-4 rounded-lg bg-indigo-500 text-xl font-medium hover:bg-indigo-600 active:ring-4 ring-0 ring-indigo-500 ring-opacity-20"
+            aria-label="Reset button"
+          >
+            Reset
+          </button>
+          <button
+            type="button"
+            onClick={decrement}
+            aria-label="Decrement button"
             className="px-4 py-2 my-4 rounded-lg bg-indigo-500 text-xl font-medium hover:bg-indigo-600 active:ring-4 ring-0 ring-indigo-500 ring-opacity-20"
           >
-            Count Is: {count}
+            Decrement
           </button>
         </p>
-        <p>
-          Edit <code className="p-1 bg-gray-700 rounded-md">App.tsx</code> and
-          save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://tailwindcss.com/docs"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tailwindcss Docs
-          </a>
-        </p>
-      </header>
+      </main>
     </div>
   );
 }
